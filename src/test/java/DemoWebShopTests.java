b600570896e9811280acf9983382e1c67571443e
Bakhtiyar Zubairov <murderer187@mail.ru>
2021-01-19 13:20:32 +0100

    Add annotations

--- src/test/java/DemoWebShopTests.java
+++ src/test/java/DemoWebShopTests.java
@@ -1,13 +1,20 @@
+import io.qameta.allure.Feature;
+import io.qameta.allure.Owner;
 import io.qameta.allure.restassured.AllureRestAssured;
 import io.restassured.RestAssured;
 import io.restassured.filter.log.RequestLoggingFilter;
 import io.restassured.filter.log.ResponseLoggingFilter;
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Tag;
 import org.junit.jupiter.api.Test;
 
 import static io.restassured.RestAssured.given;
 import static org.hamcrest.Matchers.is;
 
+@Owner("Bakhtiyar Z")
+@Feature("Subscribe button")
+@Tag("api")
 public class DemoWebShopTests {
 
   @BeforeEach
@@ -18,6 +25,7 @@ public class DemoWebShopTests {
   }
 
   @Test
+  @DisplayName("Verification to subscribe by an unauthorized user")
   void subscribeWithUnauthorizedUserTest() {
     given()
         .contentType("application/x-www-form-urlencoded; charset=UTF-8")
